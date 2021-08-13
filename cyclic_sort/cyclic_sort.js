23988dcc9e43d08713df16ff06cb2d6917ab3da7
Andy Garcia <acgarzeah@gmail.com>
2021-08-12 18:20:30 -0700

    Finished cyclic sort

---
+++ cyclic_sort/cyclic_sort.js
@@ -0,0 +1,16 @@
+const cyclic_sort = function (nums) {
+  let i = 0;
+
+  while (i < nums.length) {
+    const j = nums[i] - 1;
+
+    if (nums[i] !== nums[j]) {
+      [nums[i], nums[j]] = [nums[j], nums[i]]; // Swap
+    } else {
+      // Move up if it's at correct index
+      i++;
+    }
+  }
+
+  return nums;
+};
