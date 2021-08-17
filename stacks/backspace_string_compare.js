a35c47c524698de4825407eac6356a3d02cb2084
Andy Garcia <acgarzeah@gmail.com>
2021-08-16 22:34:12 -0700

    finished stack problem

--- stacks/backspace_string_compare.js
+++ stacks/backspace_string_compare.js
@@ -1,16 +1,16 @@
 var backspaceCompare = function (s, t) {
+  var handleBackspace = function (str) {
+    const stack = [];
+    let i = 0;
+
+    while (i < str.length) {
+      if (str[i] === "#") stack.pop();
+      else stack.push(str[i]);
+      i++;
+    }
+
+    return stack.join("");
+  };
+
   return handleBackspace(s) === handleBackspace(t);
-};
-
-var handleBackspace = function (str) {
-  const stack = [];
-  let i = 0;
-
-  while (i < str.length) {
-    if (str[i] === "#") stack.pop();
-    else stack.push(str[i]);
-    i++;
-  }
-
-  return stack.join("");
 };
