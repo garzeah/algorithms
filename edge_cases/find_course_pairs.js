d02e96fe89be4e47560d898dea916deea51c772e
Andy Garcia <acgarzeah@gmail.com>
2021-09-09 23:33:58 -0700

    Adding a question I got from karat

---
+++ edge_cases/find_course_pairs.js
@@ -0,0 +1,143 @@
+/*
+You are a developer for a university. Your current project is to develop a system for students to find courses they share with friends. The university has a system for querying courses students are enrolled in, returned as a list of (ID, course) pairs.
+
+Write a function that takes in a collection of (student ID number, course name) pairs and returns, for every pair of students, a collection of all courses they share.
+
+Sample Input:
+
+student_course_pairs_1 = [
+  ["58", "Linear Algebra"],
+  ["94", "Art History"],
+  ["94", "Operating Systems"],
+  ["17", "Software Design"],
+  ["58", "Mechanics"],
+  ["58", "Economics"],
+  ["17", "Linear Algebra"],
+  ["17", "Political Science"],
+  ["94", "Economics"],
+  ["25", "Economics"],
+  ["58", "Software Design"],
+]
+
+Sample Output (pseudocode, in any order):
+
+find_pairs(student_course_pairs_1) =>
+{
+  "58,17": ["Software Design", "Linear Algebra"]
+  "58,94": ["Economics"]
+  "58,25": ["Economics"]
+  "94,25": ["Economics"]
+  "17,94": []
+  "17,25": []
+}
+
+Sample Input:
+
+student_course_pairs_2 = [
+  ["0", "Advanced Mechanics"],
+  ["0", "Art History"],
+  ["1", "Course 1"],
+  ["1", "Course 2"],
+  ["2", "Computer Architecture"],
+  ["3", "Course 1"],
+  ["3", "Course 2"],
+  ["4", "Algorithms"]
+]
+
+
+
+Sample output:
+
+find_pairs(student_course_pairs_2) =>
+{
+  "1,0":[]
+  "2,0":[]
+  "2,1":[]
+  "3,0":[]
+  "3,1":["Course 1", "Course 2"]
+  "3,2":[]
+  "4,0":[]
+  "4,1":[]
+  "4,2":[]
+  "4,3":[]
+}
+
+Sample Input:
+student_course_pairs_3 = [
+  ["23", "Software Design"],
+  ["3", "Advanced Mechanics"],
+  ["2", "Art History"],
+  ["33", "Another"],
+]
+
+
+Sample output:
+
+find_pairs(student_course_pairs_3) =>
+{
+  "23,3": []
+  "23,2": []
+  "23,33":[]
+  "3,2":  []
+  "3,33": []
+  "2,33": []
+}
+*/
+
+const studentCoursePairs = [
+  ["58", "Linear Algebra"],
+  ["94", "Art History"],
+  ["94", "Operating Systems"],
+  ["17", "Software Design"],
+  ["58", "Mechanics"],
+  ["58", "Economics"],
+  ["17", "Linear Algebra"],
+  ["17", "Political Science"],
+  ["94", "Economics"],
+  ["25", "Economics"],
+  ["58", "Software Design"]
+];
+
+const intersection = (array1, array2) => {
+  return array1.filter((value) => array2.includes(value));
+};
+
+const findPairs = (courses) => {
+  const studentToCourses = {};
+  const result = {};
+
+  // Mapping the id and courses together
+  for (let course of courses) {
+    const { id, name } = course;
+
+    if (!studentToCourses[id]) {
+      studentToCourses[id] = [];
+    }
+
+    studentToCourses[id].push(course[name]);
+  }
+
+  // Getting the student ids to maintain order and prevent dupes
+  const studentIds = Object.keys(studentToCourses);
+
+  for (let i = 0; i < Object.keys(studentToCourses).length; i++) {
+    for (let j = i + 1; j < Object.keys(studentToCourses).length; j++) {
+      let studentId1 = studentIds[i];
+      let studentId2 = studentIds[j];
+      let studentCourses1 = studentToCourses[studentId1];
+      let studentCourses2 = studentToCourses[studentId2];
+
+      // If they do not equal then we want to find the intersect
+      if (studentId1 !== studentId2) {
+        result[`${studentId1}, ${studentId2}`] = intersection(
+          studentCourses1,
+          studentCourses2
+        );
+      }
+    }
+  }
+
+  return result;
+};
+
+console.log(findPairs(studentCoursePairs));
