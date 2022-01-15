b92a571411c9a22c4356a34facd239f9979cbeef
Andy Garcia <acgarzeah@gmail.com>
2022-01-15 11:17:25 -0800

    modified var names

--- two-pointers/trapping_rain_water.py
+++ two-pointers/trapping_rain_water.py
@@ -1,35 +1,31 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        leftMaxes, rightMaxes, minWallHeights = [], [], []
-        currLeftMax, currRightMax, total = 0, 0, 0
+        left_maxes, right_maxes, min_maxes = [], [], []
+        curr_left_max, curr_right_max, total = 0, 0, 0
 
         # Finding the left max of walls for each position
         for i in range(len(height)):
-            currLeftMax = max(currLeftMax, height[i])
-            leftMaxes.append(currLeftMax)
+            curr_left_max = max(curr_left_max, height[i])
+            left_maxes.append(curr_left_max)
 
         # Finding the right max of walls for each position
         for i in reversed(range(len(height))):
-            currRightMax = max(currRightMax, height[i])
-            rightMaxes.insert(0, currRightMax)
+            curr_right_max = max(curr_right_max, height[i])
+            right_maxes.insert(0, curr_right_max)
 
-        # Finding the min wall heights of left and right maxes
+        # Finding the min heights of left and right maxes
         for i in range(len(height)):
-            minWallHeight = min(leftMaxes[i], rightMaxes[i])
-            minWallHeights.append(minWallHeight)
+            min_max = min(left_maxes[i], right_maxes[i])
+            min_maxes.append(min_max)
 
         # Calculating the water height
         for i in range(len(height)):
-            currHeight = height[i]
-            currMinWallHeight = minWallHeights[i]
-
-            # Gives us the current water height
-            currWaterHeight = currMinWallHeight - currHeight
+            min_max, curr_height = min_maxes[i], height[i]
+            water_amount = min_max - curr_height # Gives us the current water height
 
             # Only want values > 0 meaning water is trapped
-            if currWaterHeight > 0:
-                total += currWaterHeight
-
+            if water_amount > 0:
+                total += water_amount
 
         return total
 
