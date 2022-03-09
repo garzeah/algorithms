0b967f36a049430d47e6f5549561d44963b84e9c
Andy Garcia <acgarzeah@gmail.com>
2022-03-08 19:40:56 -0800

    added new dfs prob

---
+++ dfs/maximum_depth_of_binary_tree.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        return self.dfs(root, 0)
+
+    def dfs(self, root, depth):
+        if root is None:
+            return depth
+
+        return max(self.dfs(root.left, depth + 1), self.dfs(root.right, depth + 1))
+
+# Time Complexity: O(n)
+# Space Complexity: O(n)
