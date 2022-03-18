a5d4d425fee6e34e4351e1de1d0a830ae27b0d93
Andy Garcia <acgarzeah@gmail.com>
2022-03-17 17:26:03 -0700

    updated prob

--- dfs/binary_tree_maximum_path_sum.py
+++ dfs/binary_tree_maximum_path_sum.py
@@ -5,42 +5,33 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    global_maximum_sum = float('-inf')
-
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
-        self.find_maximum_path_sum_recursive(root)
-        return self.global_maximum_sum
+        self.global_max_sum = float('-inf')
+        self.dfs(root)
+        return self.global_max_sum
 
-    def find_maximum_path_sum_recursive(self, curr_node):
+    def dfs(self, curr_node):
         if curr_node is None:
             return 0
 
-        max_path_sum_from_left = self.find_maximum_path_sum_recursive(
-          curr_node.left)
-        max_path_sum_from_right = self.find_maximum_path_sum_recursive(
-          curr_node.right)
+        max_path_sum_from_left = self.dfs(curr_node.left)
+        max_path_sum_from_right = self.dfs(curr_node.right)
 
-        # Ignore paths with negative sums, since we need to find the maximum
-        # sum we should ignore any path which has an overall negative sum.
+        # Ignore paths with negative sums, since we need to find the maximum sum we should
+        # ignore any path which has an overall negative sum.
         max_path_sum_from_left = max(max_path_sum_from_left, 0)
         max_path_sum_from_right = max(max_path_sum_from_right, 0)
 
-        # Maximum path sum at the current node will be equal to the sum from the
-        # left subtree + the sum from right subtree + val of current node
-        local_maximum_sum = max_path_sum_from_left + max_path_sum_from_right + curr_node.val
+        # Maximum path sum at the current node will be equal to the sum from the left subtree +
+        # the sum from right subtree + val of current node
+        local_max_sum = max_path_sum_from_left +  max_path_sum_from_right + curr_node.val
 
         # Update the global maximum sum
-        self.global_maximum_sum = max(self.global_maximum_sum, local_maximum_sum)
+        self.global_max_sum = max(self.global_max_sum, local_max_sum)
 
         # Maximum sum of any path from the current node will be equal to the maximum of
         # the sums from left or right subtrees plus the value of the current node
-        return max(max_path_sum_from_left, max_path_sum_from_right) + curr_node.val
+        return max(max_path_sum_from_left,  max_path_sum_from_right) + curr_node.val
 
-# Time Complexity: The time complexity of the above algorithm is O(N), where ‘N’
-# is the total number of nodes in the tree. This is due to the fact that
-# we traverse each node once.
-
-# Space Complexity: The space complexity of the above algorithm will be O(N) in
-# the worst case. This space will be used to store the recursion stack. The
-# worst case will happen when the given tree is a linked
-# list (i.e., every node has only one child).
+# Time Complexity: O(n)
+# Space Complexity: O(n)
