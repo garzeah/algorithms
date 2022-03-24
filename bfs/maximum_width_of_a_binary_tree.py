0bd4b5396060444c8aa77c5b6096ba046451af98
Andy Garcia <acgarzeah@gmail.com>
2022-03-24 13:38:48 -0700

    updated prob

--- bfs/maximum_width_of_a_binary_tree.py
+++ bfs/maximum_width_of_a_binary_tree.py
@@ -9,14 +9,14 @@ class Solution:
         if root is None:
             return 0
 
-        queue = deque([(root, 1)])
+        queue = deque([[root, 1]])
         max_width = 0
 
         while queue:
             # Getting the indexes for the first and last child in the current level
-            left_idx, right_idx = queue[0][1], queue[-1][1]
+            left, right = queue[0][1], queue[-1][1]
             # Formula for calculating the width using indexes
-            max_width = max(max_width, right_idx - left_idx + 1)
+            max_width = max(max_width, right - left + 1)
             # Used to access the first and last child of next level
             next_level = deque()
             for _ in range(len(queue)):
@@ -24,10 +24,10 @@ class Solution:
 
                 # Adding the next levels while calculating their indexes
                 if curr_node.left:
-                    next_level.append((curr_node.left, index * 2))
+                    next_level.append([curr_node.left, index * 2])
 
                 if curr_node.right:
-                    next_level.append((curr_node.right, index * 2 + 1))
+                    next_level.append([curr_node.right, index * 2 + 1])
 
             # Replacing so that we can access the first and last child
             queue = next_level
