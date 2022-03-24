79dd25534b3be067864c6dd449baa517cbcc0647
Andy Garcia <acgarzeah@gmail.com>
2022-03-23 18:37:41 -0700

    updated prob

--- dfs/binary_tree_maximum_path_sum.py
+++ dfs/binary_tree_maximum_path_sum.py
@@ -6,32 +6,32 @@
 #         self.right = right
 class Solution:
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
-        self.global_max_sum = float('-inf')
+        self.global_max = float('-inf')
         self.dfs(root)
-        return self.global_max_sum
+        return self.global_max
 
     def dfs(self, curr_node):
         if curr_node is None:
             return 0
 
-        max_path_sum_from_left = self.dfs(curr_node.left)
-        max_path_sum_from_right = self.dfs(curr_node.right)
+        max_left = self.dfs(curr_node.left)
+        max_right = self.dfs(curr_node.right)
 
         # Ignore paths with negative sums, since we need to find the maximum sum we should
         # ignore any path which has an overall negative sum.
-        max_path_sum_from_left = max(max_path_sum_from_left, 0)
-        max_path_sum_from_right = max(max_path_sum_from_right, 0)
+        max_left = max(max_left, 0)
+        max_right = max(max_right, 0)
 
         # Maximum path sum at the current node will be equal to the sum from the left subtree +
         # the sum from right subtree + val of current node
-        local_max_sum = curr_node.val + max_path_sum_from_left +  max_path_sum_from_right
+        local_max = curr_node.val + max_left +  max_right
 
         # Update the global maximum sum
-        self.global_max_sum = max(self.global_max_sum, local_max_sum)
+        self.global_max = max(self.global_max, local_max)
 
         # Maximum sum of any path from the current node will be equal to the maximum of
         # the sums from left or right subtrees plus the value of the current node
-        return curr_node.val + max(max_path_sum_from_left,  max_path_sum_from_right)
+        return curr_node.val + max(max_left,  max_right)
 
 # Time Complexity: O(n)
 # Space Complexity: O(n)
