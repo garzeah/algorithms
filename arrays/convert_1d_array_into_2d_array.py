5fd4c6ea317201bfdf8f44acd98958d857c5e601
Andy Garcia <acgarzeah@gmail.com>
2022-03-27 19:53:48 -0700

    updated prob

--- arrays/convert_1d_array_into_2d_array.py
+++ arrays/convert_1d_array_into_2d_array.py
@@ -1,13 +1,13 @@
 class Solution:
     def construct2DArray(self, original: List[int], m: int, n: int) -> List[List[int]]:
         output = []
-        if len(original) != m*n:
+        if len(original) != m * n:
             return output
 
         # For every n steps in our array...
         for i in range(0, len(original), n):
             # We want to append an array of n elements
-            output.append(original[i:i+n])
+            output.append(original[i : i + n])
 
         return output
 
--- bit_manipulation/flipping_an_image.py
+++ bit_manipulation/flipping_an_image.py
@@ -1,11 +1,11 @@
 class Solution:
     def flipAndInvertImage(self, image: List[List[int]]) -> List[List[int]]:
-        n = len(image)
+        n = len(image[0])
 
         for row in image:
             for i in range((n + 1) // 2): # Getting the amount of times we need to swap
                 # Swapping the values and getting the inverted image using XOR
-                row[i], row[n - i - 1] = row[n - i - 1] ^ 1, row[i] ^ 1
+                row[i], row[n - 1 - i] = row[n - 1 - i] ^ 1, row[i] ^ 1
 
         return image
 
