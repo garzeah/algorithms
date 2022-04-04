e4544cd2d2869dd073cc7253081a41499015e972
Andy Garcia <acgarzeah@gmail.com>
2022-04-04 13:17:06 -0700

    added new heap probs

---
+++ heaps/reorganize_string.py
@@ -0,0 +1,30 @@
+class Solution:
+    def reorganizeString(self, str: str) -> str:
+        freq_map, max_heap, output = {}, [], []
+
+        for char in str:
+            freq_map[char] = freq_map.get(char, 0) + 1
+
+        # Add all characters to the max heap
+        for char, frequency in freq_map.items():
+            heappush(max_heap, (-frequency, char))
+
+        prev_char, prev_frequency = None, 0
+        while max_heap:
+            frequency, char = heappop(max_heap)
+            # Add the previous entry back in the heap if its frequency is greater than zero
+            if prev_char and -prev_frequency > 0:
+                heappush(max_heap, (prev_frequency, prev_char))
+            # Append the current character to the result string and decrement its count
+            output.append(char)
+            prev_char = char
+            prev_frequency = frequency + 1  # Decrement the frequency
+
+        # If we were successful in appending all the characters to the result string, return it
+        return ''.join(output) if len(output) == len(str) else ""
+
+# Time Complexity: The time complexity of the above algorithm is O(N*logN)
+# where ‘N’ is the number of characters in the input string.
+
+# Space Complexity: The space complexity will be O(N), as in the worst case,
+# we need to store all the ‘N’ characters in the HashMap.
---
+++ heaps/task_scheduler.py
@@ -0,0 +1,39 @@
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        freq_map, max_heap, interval_count = {}, [], 0
+
+        for char in tasks:
+            freq_map[char] = freq_map.get(char, 0) + 1
+
+        # Add all tasks to the max heap
+        for char, frequency in freq_map.items():
+            heappush(max_heap, (-frequency, char))
+
+        while max_heap:
+            wait_list = []
+            tasks = n + 1  # Try to execute as many as 'k+1' tasks from the max-heap
+            while tasks > 0 and max_heap:
+                interval_count += 1
+                frequency, char = heappop(max_heap)
+                if -frequency > 1:
+                    # Decrement the frequency and add to the wait_list
+                    wait_list.append((frequency+1, char))
+                tasks -= 1
+
+            # Put all the waiting list back on the heap
+            for frequency, char in wait_list:
+                heappush(max_heap, (frequency, char))
+
+            if max_heap:
+                interval_count += tasks # We'll be having 'n' idle intervals for the next iteration
+
+        return interval_count
+
+# Time Complexity: The time complexity of the above algorithm is O(N*logN)
+# where ‘N’ is the number of tasks. Our while loop will iterate once for
+# each occurrence of the task in the input (i.e. ‘N’) and in each iteration
+# we will remove a task from the heap which will take O(logN) time. Hence
+# the overall time complexity of our algorithm is O(N*logN).
+
+# Space Complexity: The space complexity will be O(N), as in the
+# worst case, we need to store all the ‘N’ tasks in the HashMap.
