b60e9a34ff7b621ff99604e4439f5f957f5cd816
Andy Garcia <acgarzeah@gmail.com>
2022-04-15 08:36:44 -0700

    updated probs

---
+++ backtracking/permutations_1.py
@@ -0,0 +1,28 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        curr_path, output = [], []
+        self.backtrack(nums, curr_path, output)
+        return output
+
+    def backtrack(self, nums, curr_path, output):
+        if len(curr_path) == len(nums):
+            output.append(list(curr_path))
+            return
+
+        for i in range(len(nums)):
+            # If number is in our current permutation, don't add it
+            if nums[i] in curr_path:
+                continue
+
+            # Otherwise, if it is not in our current permutation, add it
+            curr_path.append(nums[i])
+            self.backtrack(nums, curr_path, output)
+            curr_path.pop()
+
+# Time Complexity: We know that there are a total of N! permutations of a set with
+# ‘N’ numbers. We are also interating through nums for each path which is O(N),
+# which makes the overall time complexity of our algorithm O(N*N!).
+
+# Space Complexity: There are a total of N! permutations with N being the length of
+# the elements. Since we have to create a list for each permutation, the space
+# complexity will be O(N*N!)
