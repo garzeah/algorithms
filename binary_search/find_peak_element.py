17c7e26c3a2d457de4b1a553ee4f88b62c96beac
Andy Garcia <acgarzeah@gmail.com>
2022-04-25 18:07:02 -0700

    updated prob

--- binary_search/find_peak_element.py
+++ binary_search/find_peak_element.py
@@ -2,9 +2,10 @@ class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
         start, end = 0, len(nums) - 1
 
-        # We are using < because if we use <= we'll go out
-        # of bounds for inputs with length that is less
-        # than 3 since we're check the siblings of mid
+        # When we use < instead of <=, our start/end will
+        # be equal instead, and we can use that to return
+        # where the midpoint would have been if it were <=.
+        # Typically good if we're comparing a midpoint to its sibs.
         while start < end:
             mid = (start + end) // 2
 
