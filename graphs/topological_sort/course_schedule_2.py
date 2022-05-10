881851d025009846257b1cfa69b3c6553f2fb558
Andy Garcia <acgarzeah@gmail.com>
2022-05-10 12:51:00 -0700

    updated probs

--- graphs/topological_sort/course_schedule_2.py
+++ graphs/topological_sort/course_schedule_2.py
@@ -1,17 +1,17 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        in_degree, graph, sorted_order = {}, {}, []
+        in_degree, adj, sorted_order = {}, {}, []
         if numCourses <= 0:
             return sorted_order
 
-        # a. Initialize the graph
+        # a. Initializing incoming degrees and adjancey list
         for i in range(numCourses):
             in_degree[i] = 0 # Count of incoming prerequisites
-            graph[i] = [] # Adjacency list graph
+            adj[i] = [] # Adjacency list
 
-        # b. Build the graph
+        # b. Build the adj
         for child, parent in prerequisites:
-            graph[parent].append(child)  # Put the child into it's parent's list
+            adj[parent].append(child)  # Put the child into it's parent's list
             in_degree[child] += 1  # Increment child's in_degree
 
         # c. Find all sources i.e., all numCourses with 0 in-degrees
@@ -20,16 +20,19 @@ class Solution:
             if in_degree[key] == 0:
                 sources.append(key)
 
-        # d. For each source, add it to the sorted_order and subtract one from
-        # all of its children's in-degrees if a child's in-degree becomes zero,
-        # add it to the sources queue
+        # d. For each source, add it to the sorted_order and subtract
+        # one from all of its children's in-degrees if a child's
+        # in-degree becomes zero, add it to the sources queue
         while sources:
             vertex = sources.popleft()
             sorted_order.append(vertex)
 
-            # Get the node's children to decrement their in-degrees
-            for child in graph[vertex]:
+            # Want to decrement the incoming degrees
+            # in a vertex to get the sorted order
+            for child in adj[vertex]:
                 in_degree[child] -= 1
+                # When a vertex has 0 in-degrees, it becomes
+                # a source so add it into our queue
                 if in_degree[child] == 0:
                     sources.append(child)
 
