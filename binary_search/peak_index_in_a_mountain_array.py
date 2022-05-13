f4bb6d1c19bc09a6b4b0689be1c6bb63b7712da4
Andy Garcia <acgarzeah@gmail.com>
2022-05-12 18:08:32 -0700

    updated prob

--- binary_search/peak_index_in_a_mountain_array.py
+++ binary_search/peak_index_in_a_mountain_array.py
@@ -3,7 +3,7 @@ class Solution:
         start, end = 0, len(arr) - 1
 
         # We are using < because if we use <= we'll go out
-        # of bounds since we're check the siblings of mid
+        # of bounds since we're check the adj. pos. of mid
         while start < end:
             mid = (start + end) // 2
 
@@ -11,13 +11,14 @@ class Solution:
             # start and end sides then we have found a peak
             if arr[mid] > arr[mid + 1] and arr[mid] > arr[mid - 1]:
                 return mid
+            # Move it up since there's a bigger value on the right side
             elif arr[mid] < arr[mid + 1]:
                 start = mid + 1
             else:
                 end = mid - 1
 
-        # Can return start or end since we are using
-        # less than in a mountain array
+        # Since we are using less than rather than <= both
+        # start and end will finish at the same index
         return end
 
 # Time Complexity: O(logN)
