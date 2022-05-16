88f667abc11702adc167033a8eb0cea296268efb
Andy Garcia <acgarzeah@gmail.com>
2022-05-16 16:58:48 -0700

    updated probs

--- backtracking/word_search.py
+++ backtracking/word_search.py
@@ -1,44 +1,46 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        rows, cols = len(board), len(board[0])
-        curr_path = set() # Since we don't revisit the same pos
+        ROWS, COLS = len(board), len(board[0])
+        visited = set() # Since we don't revisit the same pos
 
-        def dfs(row, col, i):
+        def backtrack(row, col, i):
+            # Base case for when we find the word
             if i == len(word):
                 return True
 
+            # Base case for when we don't find the word
             if (
-                row < 0 or col < 0 or # Out of bounds
-                row >= rows or col >= cols or # Out of bounds
-                word[i] != board[row][col] or # Not matching
-                (row, col) in curr_path # Already visited
+                (row, col) in visited or # Already visited
+                row < 0 or row >= ROWS or # Out of bounds
+                col < 0 or col >= COLS or # Out of bounds
+                board[row][col] != word[i] # Not matching
             ):
                 return False
 
 
-            curr_path.add((row, col))
+            visited.add((row, col))
 
-            # Check adjacent positions for our word
+            # Checking adjacent positions for our word
             output = (
-                dfs(row + 1, col, i + 1) or # Right
-                dfs(row - 1, col, i + 1) or # Left
-                dfs(row, col + 1, i + 1) or # Up
-                dfs(row, col - 1, i + 1) # Down
+                backtrack(row + 1, col, i + 1) or # Right
+                backtrack(row - 1, col, i + 1) or # Left
+                backtrack(row, col + 1, i + 1) or # Up
+                backtrack(row, col - 1, i + 1) # Down
             )
 
-            curr_path.remove((row, col))
+            visited.remove((row, col))
 
             return output
 
-        for row in range(rows):
-            for col in range(cols):
-                if dfs(row, col, 0):
+        for row in range(ROWS):
+            for col in range(COLS):
+                if backtrack(row, col, 0):
                     return True
 
         return False
 
 # Time Complexity: O(n * m * 4^n) where n is the number of columns
-# and m in the number of rows. For some positions, we will have
+# and m in the number of ROWS. For some positions, we will have
 # at most 4 possible choices to backtrack from.
 
 # Space Complexity: O(1) since we are just returning a bool value.
