67ee85d3957c2018a1213b71ac5d0f97ef8dc790
Andy Garcia <acgarzeah@gmail.com>
2022-05-16 14:17:47 -0700

    updated probs

--- binary_search/find_minimum_in_rotated_sorted_array_2.py
+++ binary_search/find_minimum_in_rotated_sorted_array_2.py
@@ -1,18 +1,35 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
         start, end = 0, len(nums) - 1
+        min_num = float('inf')
 
-        while start < end:
-            mid = (start + end) // 2
-
-            if nums[mid] > nums[end]:
-                start = mid + 1
-            elif nums[mid] < nums[end]:
-                end = mid
-            else:
+        while start <= end:
+            while start < end and nums[start] == nums[start + 1]:
+                start += 1
+            while start < end and nums[end] == nums[end - 1]:
                 end -= 1
 
-        return nums[start]
+            # If we get to a subarray that is already sorted then
+            # we can update our result to the min of itself
+            # and break out of the loop
+            if nums[start] < nums[end]:
+                min_num = min(min_num, nums[start])
+                break
 
-# Time Complexity: O(log(n))
+            mid = (start + end) // 2
+            min_num = min(min_num, nums[mid])
+
+            # If it is part the of left sorted portion so
+            # we want to search the right sorted portion
+            if nums[mid] >= nums[start]:
+                start = mid + 1
+            # If it is part the of right sorted portion do
+            # we want to search the left sorted portion
+            else:
+                end = mid - 1
+
+        return min_num
+
+# Time Complexity: O(logn)
 # Space Complexity: O(1)
+# Solution: https://www.youtube.com/watch?v=nIVW4P8b1VA
