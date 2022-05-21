4b8caf97f9acfab2403d71f53316e3e379900e80
Andy Garcia <acgarzeah@gmail.com>
2022-05-20 23:32:53 -0700

    added new prob

---
+++ two_pointers/same_and_opposite/valid_palindrome.py
@@ -0,0 +1,31 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+        left, right = 0, len(s) - 1
+
+        while left < right:
+            # Sliding left is not alphanumeric
+            if not s[left].isalnum():
+                left += 1
+                continue
+
+            # Sliding right is not alphanumeric
+            if not s[right].isalnum():
+                right -= 1
+                continue
+
+            # Not a palindrome
+            if s[left] != s[right]:
+                return False
+
+            # Checking next letter
+            if s[left] == s[right]:
+                left += 1
+                right -= 1
+
+        return True
+
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+
+
