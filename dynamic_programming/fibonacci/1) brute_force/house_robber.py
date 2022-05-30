b8b0a1f0e672afcc73b5d1f73c5e7d023ecc8f71
Andy Garcia <acgarzeah@gmail.com>
2022-05-30 16:34:41 -0700

    updated probs

---
+++ dynamic_programming/fibonacci/1) brute_force/house_robber.py
@@ -0,0 +1,20 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        return self.find_max_steal_recursive(nums, 0)
+
+
+    def find_max_steal_recursive(self, nums, start):
+        # Have no more houses to rob from
+        if start >= len(nums):
+            return 0
+
+        # Steal from current house and skip one to steal next
+        steal_current = nums[start] + self.find_max_steal_recursive(nums, start + 2)
+
+        # Skip current house to steal from the adjacent house
+        skip_current = self.find_max_steal_recursive(nums, start + 1)
+
+        return max(steal_current, skip_current)
+
+# Time Complexity: O(2^n)
+# Space Compelxity: O(n) which is used to store the recursion stack
