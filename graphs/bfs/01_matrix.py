cd8aca62e423bec9556fe5d69047cb24a5c167dd
Andy Garcia <acgarzeah@gmail.com>
2022-06-01 19:37:52 -0700

    updated probs

---
+++ graphs/bfs/01_matrix.py
@@ -0,0 +1,38 @@
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        ROWS, COLS = len(mat), len(mat[0])
+        queue = deque()
+
+        for row in range(ROWS):
+            for col in range(COLS):
+                # Find all the 0s and append to our queue
+                # since we will use those for searching
+                if mat[row][col] == 0:
+                    queue.append((row, col))
+                # Set to "T" so when we find it when doing
+                # BFS we can add the previous position
+                # with 1 iteratively
+                else:
+                    mat[row][col] = "T"
+
+        directions = [[1,0], [-1,0], [0,1], [0,-1]]
+        while queue:
+            row, col = queue.popleft()
+
+            for x, y in directions:
+                r, c = row + x, col + y
+
+                if (
+                    r in range(ROWS) and
+                    c in range(COLS) and
+                    mat[r][c] == "T"
+                ):
+                    # Adds the previous everytime we find a "T"
+                    mat[r][c] = mat[row][col] + 1
+                    queue.append((r, c))
+
+        return mat
+
+# Time Complexity: O(n * m)
+# Space Complexity: O(n * m)
+# Solution: https://www.youtube.com/watch?v=hIvMZFqjs_A
