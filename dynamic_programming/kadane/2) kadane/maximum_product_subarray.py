fef3bb16b7d247555258eaa6aa5b9f4dda445c37
Andy Garcia <acgarzeah@gmail.com>
2022-06-12 00:56:06 -0700

    updated probs

---
+++ dynamic_programming/kadane/2) kadane/maximum_product_subarray.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        prev_min = prev_max = global_max = nums[0]
+
+        # Because of an edge case where when we have an array of negative numbers
+        # it'll yield a sequence of negative and positive numbers. In order to
+        # find the maximum values, we need to keep track of the minimum values
+        # as they can yield the maximum values.
+        for i in range(1, len(nums)):
+            curr_min = min(prev_max * nums[i], prev_min * nums[i], nums[i])
+            curr_max = max(prev_max * nums[i], prev_min * nums[i], nums[i])
+            global_max = max(global_max, curr_max)
+            prev_max = curr_max
+            prev_min = curr_min
+
+        return global_max
+
+# Time Complexity: O(n)
+# Space Compelxity: O(1)
+# Solution: https://www.youtube.com/watch?v=lXVy6YWFcRM
---
+++ dynamic_programming/kadane/2) kadane/maximum_subarray.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        curr_sum = max_sum = nums[0]
+
+        for i in range(1, len(nums)):
+            # We have two choices we need to consider which is, is
+            # it better to start a new subarray or is it better
+            # to keep building the contiguous subarray
+            curr_sum = max(nums[i], nums[i] + curr_sum)
+            max_sum = max(max_sum, curr_sum)
+
+        return max_sum
+
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+# Solution: https://www.youtube.com/watch?v=2MmGzdiKR9Y
