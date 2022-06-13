5a682f26447b73398a83f9b630d56ef560492fae
Andy Garcia <acgarzeah@gmail.com>
2022-06-12 20:22:42 -0700

    updated probs

--- dynamic_programming/0_1_knapsack/2) top_down/partition_equal_subset_sum.py
+++ dynamic_programming/0_1_knapsack/2) top_down/partition_equal_subset_sum.py
@@ -1,34 +1,35 @@
 class Solution:
-    def can_partition(self, num):
-        s = sum(num)
+    def canPartition(self, nums):
+        target = sum(nums)
 
-        # if 's' is a an odd number, we can't have two subsets with equal sum
-        if s % 2 != 0:
+        # if 'target' is a an odd number, we can't have two subsets with equal sum
+        if target % 2 == 1:
             return False
 
         # initialize the 'dp' array, -1 for default, 1 for true and 0 for false
-        dp = [[-1 for x in range(int(s/2)+1)] for y in range(len(num))]
-        return True if self.helper(dp, num, int(s / 2), 0) == 1 else False
+        target //= 2
+        dp = [[-1 for x in range(target + 1)] for y in range(len(nums))]
+        return True if self.helper(dp, nums, target, 0) == 1 else False
 
-    def helper(self, dp, num, sum, currentIndex):
+    def helper(self, dp, nums, target, idx):
         # base check
-        if sum == 0:
+        if target == 0:
             return 1
 
-        n = len(num)
-        if n == 0 or currentIndex >= n:
+        n = len(nums)
+        if n == 0 or idx >= n:
             return 0
 
         # if we have not already processed a similar problem
-        if dp[currentIndex][sum] == -1:
-            # recursive call after choosing the number at the currentIndex
-            # if the number at currentIndex exceeds the sum, we shouldn't process this
-            if num[currentIndex] <= sum:
-                if self.helper(dp, num, sum - num[currentIndex], currentIndex + 1) == 1:
-                    dp[currentIndex][sum] = 1
+        if dp[idx][target] == -1:
+            # recursive call after choosing the number at the idx
+            # if the number at idx exceeds the target, we shouldn't process this
+            if nums[idx] <= target:
+                if self.helper(dp, nums, target - nums[idx], idx + 1) == 1:
+                    dp[idx][target] = 1
                     return 1
 
-            # recursive call after excluding the number at the currentIndex
-            dp[currentIndex][sum] = self.helper(dp, num, sum, currentIndex + 1)
+            # recursive call after excluding the number at the idx
+            dp[idx][target] = self.helper(dp, nums, target, idx + 1)
 
-        return dp[currentIndex][sum]
+        return dp[idx][target]
--- dynamic_programming/0_1_knapsack/3) bottom_up/partition_equal_subset_sum.py
+++ dynamic_programming/0_1_knapsack/3) bottom_up/partition_equal_subset_sum.py
@@ -10,13 +10,12 @@ class Solution:
         # We are trying to find a subset of given numbers
         # that has a total sum of 'target/2'.
         target = target // 2
-        n = len(nums)
         # x represents target and y represents num/sums
-        dp = [[False for x in range(target + 1)] for y in range(n)]
+        dp = [[False for x in range(target + 1)] for y in range(len(nums))]
 
         # Populate the sum = 0 column, as we can always have '0' sum
         # without including any element
-        for i in range(0, n):
+        for i in range(0, len(nums)):
             dp[i][0] = True
 
         # Process the first row since with only one number, we can form
@@ -25,7 +24,7 @@ class Solution:
             dp[0][target] = nums[0] == target
 
         # Process all subsets for all sums
-        for idx in range(1, n):
+        for idx in range(1, len(nums)):
             for target in range(1, target + 1):
                 # Check the previous to see if we can get the sum
                 # 'target' without the number at index 'idx'
@@ -36,7 +35,7 @@ class Solution:
                     dp[idx][target] = dp[idx - 1][target - nums[idx]]
 
         # The bottom-right corner will have our answer.
-        return dp[n - 1][target]
+        return dp[len(nums) - 1][target]
 
 # The above solution has time and space complexity of O(N*S), where ‘N’
 # represents total numbers and ‘S’ is the total sum of all the numbers.
