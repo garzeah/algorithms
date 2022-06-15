e226662437e04c3b2508d1635cc6548dbdfaed0f
Andy Garcia <acgarzeah@gmail.com>
2022-06-15 14:28:28 -0700

    updated prob

--- arrays/intervals/non_overlapping_intervals.py
+++ arrays/intervals/non_overlapping_intervals.py
@@ -4,15 +4,15 @@ class Solution:
         prev_end = intervals[0][1] # Keep track of first end value
         output = 0
 
-        for start, end in intervals[1:]:
-            if start >= prev_end: # Not overlapping
-                prev_end = end
+        for curr_start, curr_end in intervals[1:]:
+            if curr_start >= prev_end: # Not overlapping
+                prev_end = curr_end # Checking next end
             else: # Overlapping
                 output += 1
                 # We want to keep the value that ends first since
                 # if we keep the value that ends last, it will
                 # increase the likelihood of an overlap
-                prev_end = min(prev_end, end)
+                prev_end = min(prev_end, curr_end)
 
         return output
 
