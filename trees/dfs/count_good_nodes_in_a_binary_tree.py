abe4d7e5eb26a92c15f2840a3736edc3c5015bdb
Andy Garcia <acgarzeah@gmail.com>
2022-06-15 14:57:34 -0700

    update probs

---
+++ trees/dfs/count_good_nodes_in_a_binary_tree.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        return self.dfs(root, root.val)
+
+    def dfs(self, curr, max_val):
+        if curr is None:
+            return 0
+
+        # Only a good node if the current value is
+        # greater than or equal to the current max
+        # as we traverse along the path
+        res = 1 if curr.val >= max_val else 0
+
+        left = self.dfs(curr.left, max(curr.val, max_val))
+        right = self.dfs(curr.right, max(curr.val, max_val))
+
+        return res + left + right
+
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+# Solution: https://www.youtube.com/watch?v=7cp5imvDzl4
