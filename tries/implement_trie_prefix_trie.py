e9e09416b0bfa2a7ff182c0d3f59f266538b2dd0
Andy Garcia <acgarzeah@gmail.com>
2022-06-18 02:37:05 -0700

    updated probs

--- tries/implement_trie_prefix_trie.py
+++ tries/implement_trie_prefix_trie.py
@@ -4,54 +4,61 @@ class TrieNode:
         self.end_of_word = False
 
 class Trie:
+
     def __init__(self):
         self.root = TrieNode()
 
+
     def insert(self, word: str) -> None:
-        curr_node = self.root
+        curr = self.root
 
         for char in word:
-            # If character not in trie, create new node
-            if char not in curr_node.children:
-                curr_node.children[char] = TrieNode()
+            # Create a TrieNode for the letter if it doesn't exist
+            if char not in curr.children:
+                curr.children[char] = TrieNode()
 
-            # Check the next node (letter)
-            curr_node = curr_node.children[char]
+            curr = curr.children[char] # Moving to next letter
 
-        # For the last node, update the end of word flag
-        curr_node.end_of_word = True
+        curr.end_of_word = True
+
 
     def search(self, word: str) -> bool:
-        curr_node = self.root
+        curr = self.root
 
         for char in word:
-            # If character not in trie, it doesn't exist
-            if char not in curr_node.children:
+            if char not in curr.children:
                 return False
 
-            # Check the next node (letter)
-            curr_node = curr_node.children[char]
+            curr = curr.children[char]
 
-        return curr_node.end_of_word
+        return curr.end_of_word
+
 
     def startsWith(self, prefix: str) -> bool:
-        curr_node = self.root
+        curr = self.root
 
         for char in prefix:
-            # If character not in trie, it doesn't start with
-            if char not in curr_node.children:
+            if char not in curr.children:
                 return False
 
-            # Check the next node (letter)
-            curr_node = curr_node.children[char]
+            curr = curr.children[char]
 
         return True
 
-# Time Complexity:
-# insert(word) is O(n * m) where n is the number of words and m is the max length
-# search(word) is O(m) where m is the max length
-# startsWith(prefix) is O(m) where m is the max length
 
-# Space Complexity: O(n * m) where n is the number of words and m is the max length.
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
+
+# Time Complexity:
+# - insert(word): O(n) where n is the length of the word
+# - search(word): O(n) where n is the length of the word
+# - startsWith(prefix): O(n) where n is the length of the word
+
+# Space Complexity: O(n * m) where n is the number of words
+# and m is the max length. This would cover all of the
+# possible words contained in our trie.
 
 # Solution: https://www.youtube.com/watch?v=oobqoCJlHA0
