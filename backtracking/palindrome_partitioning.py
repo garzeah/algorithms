b8f08658aca1ced50f71725c77cec9626e4afae1
Andy Garcia <acgarzeah@gmail.com>
2022-06-21 10:29:45 -0700

    added probs

--- backtracking/palindrome_partitioning.py
+++ backtracking/palindrome_partitioning.py
@@ -1,9 +1,9 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
         output = []
-        return self.dfs(s, output, [], 0)
+        return self.backtrack(s, output, [], 0)
 
-    def dfs(self, s, output, curr_path, start):
+    def backtrack(self, s, output, curr_path, start):
         # When we reach the last index append the results
         if start >= len(s):
             output.append(list(curr_path))
@@ -13,19 +13,25 @@ class Solution:
         # palindrome then add the remaining letter in and
         # check if it is a palindrome as well
         for end in range(start, len(s)):
-            if self.isPalindrome(s, start, end):
-                curr_path.append(s[start:end+1])
-                self.dfs(s, output, curr_path, end + 1)
+            if self.is_palindrome(s[start:end + 1]):
+                curr_path.append(s[start:end + 1])
+                # We pass in end + 1 instead of start + 1 because
+                # we are partitioning the word and want to grab
+                # each sequence of it
+                self.backtrack(s, output, curr_path, end + 1)
                 curr_path.pop()
 
         return output
 
-    def isPalindrome(self, s, start, end):
-        while start < end:
+    def is_palindrome(self, s):
+        start, end = 0, len(s) - 1
+
+        while start <= end:
             if s[start] != s[end]:
                 return False
 
-            start, end = start + 1, end - 1
+            start += 1
+            end -= 1
 
         return True
 
