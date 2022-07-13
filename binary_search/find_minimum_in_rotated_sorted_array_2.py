3c3b7032df3dc203ae183d0afb3e2ab00cdddb0c
Andy Garcia <acgarzeah@gmail.com>
2022-07-12 21:00:14 -0700

    updated probs

--- binary_search/find_minimum_in_rotated_sorted_array_2.py
+++ binary_search/find_minimum_in_rotated_sorted_array_2.py
@@ -9,19 +9,17 @@ class Solution:
             while start < end and nums[end] == nums[end - 1]:
                 end -= 1
 
-            # In the event we do not have a rotated sorted array,
-            # then we want to have this check in place to get
-            # the minimum
-            if nums[start] < nums[end]:
-                min_num = min(min_num, nums[start])
-                break
-
             mid = (start + end) // 2
             min_num = min(min_num, nums[mid])
 
+            # In the event we do not have a rotated sorted array,
+            # anymore then we want to get the minimum value
+            if nums[start] < nums[end]:
+                min_num = min(min_num, nums[start])
+                break
             # If it is part the of left sorted portion so
             # we want to search the right sorted portion
-            if nums[mid] >= nums[start]:
+            elif nums[mid] >= nums[start]:
                 start = mid + 1
             # If it is part the of right sorted portion do
             # we want to search the left sorted portion
