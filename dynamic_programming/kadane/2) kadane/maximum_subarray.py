669f6e8d6329a89bb833f2edb67296a8af46711c
Andy Garcia <acgarzeah@gmail.com>
2022-07-16 01:25:39 -0700

    update probs

--- dynamic_programming/kadane/2) kadane/maximum_subarray.py
+++ dynamic_programming/kadane/2) kadane/maximum_subarray.py
@@ -3,9 +3,9 @@ class Solution:
         curr_sum = max_sum = nums[0]
 
         for i in range(1, len(nums)):
-            # We have two choices we need to consider which is, is
-            # it better to start a new subarray or is it better
-            # to keep building the contiguous subarray
+            # We have two choices for each index we need to consider
+            # which is, is it better to start a new subarray or is
+            # it better to keep building the contiguous subarray
             curr_sum = max(nums[i], nums[i] + curr_sum)
             max_sum = max(max_sum, curr_sum)
 
