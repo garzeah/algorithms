975f2786619964afc936a69ca85d43232b078cef
Andy Garcia <acgarzeah@gmail.com>
2022-07-20 12:42:58 -0700

    updated probs

---
+++ greedy/two_city_scheduling.py
@@ -0,0 +1,34 @@
+class Solution:
+    def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+        # Why greedy?
+        # If we were to take the difference of city b and a,
+        # we could use that to determine the minimum costs
+        # since this tells us how much we would save going
+        # to city a instead of b
+
+        # In order to find the minimum cost, a way we can approach
+        # this is to find the difference between city b and city a
+        # and then sort it, and grab the first n values for city b
+        # in calculating minimum costs
+        diffs = []
+        for a, b in costs:
+            # Tells us how much we would save going to city a than b
+            diffs.append([b - a, a, b])
+
+        diffs.sort()
+
+        # Grabbing the first n values for city b since we wouldn't
+        # save that much going to city a since it is sorted which
+        # is why we we grab the first n values for city b
+        res = 0
+        for i in range(len(diffs)):
+            if i < len(diffs) // 2:
+                res += diffs[i][2]
+            else:
+                res += diffs[i][1]
+
+        return res
+
+# Time Complexity: O(nlogn)
+# Space Complexity: O(n)
+# Solution: https://www.youtube.com/watch?v=d-B_gk_gJtQ
