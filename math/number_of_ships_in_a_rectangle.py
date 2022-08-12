8809c68b3df98c384e7c3ac2d3f3ba227eac91b5
Andy Garcia <acgarzeah@gmail.com>
2022-08-12 18:48:21 -0400

    updated probs

--- math/number_of_ships_in_a_rectangle.py
+++ math/number_of_ships_in_a_rectangle.py
@@ -19,7 +19,8 @@ class Solution:
 
             # Base case for when we are out of bounds or when there aren't any ships
             if (
-                topRight.x < bottomLeft.x or topRight.y < bottomLeft.y or
+                topRight.x < bottomLeft.x or
+                topRight.y < bottomLeft.y or
                 sea.hasShips(topRight, bottomLeft) is False
             ):
                 return 0
@@ -29,20 +30,21 @@ class Solution:
             midY = (bottomLeft.y + topRight.y) // 2
             mid = Point(midX, midY)
 
-            # We are going to perform our search in 4 separate quadrants because let's say
-            # we have a 10*10 rectangle, that means there are 100 points to look at total
-            # and we can keep dividing the quadrants to 4ths, 16ths, 64ths, etc. and if
-            # we do that we can consider that log base 4 of any number will be small
-            # considering that there are at most 10 ships and it'd be easier to
-            # search in that manner
+            # Since we are limited to having a 1000 then that means the maximum
+            # area we have to search for each ship is 1 million. However, we
+            # have to consider that there are at most 10 ships and we cannot
+            # make more than 400 calls. A good way to perform our search is
+            # in quadrants of 4 because if we do log base 4 of 1 million
+            # we end up with a value that approximately equals 10.
 
-            # Adding 1 to prevent overlapping boundaries
+            # Moving topLeftQ and topRightQ to prevent overlap with the bottom
+            # quadrants and moving botRightQ to prevent overlap with the botLeftQ
             topLeftQ = findShips(Point(mid.x, topRight.y), Point(bottomLeft.x, mid.y + 1))
             topRightQ = findShips(topRight, Point(mid.x + 1, mid.y + 1))
-            botRightQ = findShips(Point(topRight.x, mid.y), Point(mid.x + 1, bottomLeft.y))
             botLeftQ = findShips(mid, bottomLeft)
+            botRightQ = findShips(Point(topRight.x, mid.y), Point(mid.x + 1, bottomLeft.y))
 
-            return topLeftQ + topRightQ + botRightQ + botLeftQ
+            return topLeftQ + topRightQ + botLeftQ + botRightQ
 
         return findShips(topRight, bottomLeft)
 
