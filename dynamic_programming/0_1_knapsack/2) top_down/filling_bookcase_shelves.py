a20cab200155c474cf6c1379f9a8928ad690e18a
Andy Garcia <acgarzeah@gmail.com>
2022-08-12 18:13:41 -0400

    updated prob

--- dynamic_programming/0_1_knapsack/2) top_down/filling_bookcase_shelves.py
+++ dynamic_programming/0_1_knapsack/2) top_down/filling_bookcase_shelves.py
@@ -2,9 +2,9 @@ class Solution:
     def minHeightShelves(self, books: List[List[int]], shelfWidth: int) -> int:
         self.shelfWidth = shelfWidth # For when we want to fill a new bookshelf
         dp = [[-1 for x in range(shelfWidth + 1)] for y in range(len(books))]
-        return self.helper(books, 0, shelfWidth, 0, dp)
+        return self.helper(books, shelfWidth, 0, 0, dp)
 
-    def helper(self, books, i, width, height, dp):
+    def helper(self, books, width, height, i, dp):
         if i >= len(books):
             return height
 
@@ -12,11 +12,10 @@ class Solution:
             # Choice1: Fill current bookshelf
             choice1 = float('inf')
             if books[i][0] <= width:
-                choice1 = self.helper(books, i + 1, width - books[i][0], max(height,books[i][1]), dp)
+                choice1 = self.helper(books, width - books[i][0], max(height,books[i][1]), i + 1, dp)
 
-            # Choice-2: Start filling new book-shelf with
-            # previous height considered
-            choice2 = height + self.helper(books, i + 1, self.shelfWidth - books[i][0], books[i][1], dp)
+            # Choice-2: Start filling new book-shelf with previous height considered
+            choice2 = height + self.helper(books, self.shelfWidth - books[i][0], books[i][1], i + 1, dp)
 
 
             dp[i][width] = min(choice1, choice2)
