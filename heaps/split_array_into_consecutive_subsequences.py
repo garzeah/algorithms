156615e7117f62a55107ab3f1fb347f8cc3cb2f8
Andy Garcia <acgarzeah@gmail.com>
2022-08-13 18:15:00 -0400

    updated probs

--- heaps/split_array_into_consecutive_subsequences.py
+++ heaps/split_array_into_consecutive_subsequences.py
@@ -1,24 +1,24 @@
 class Solution:
     def isPossible(self, nums: List[int]) -> bool:
-        freq = Counter(nums) # Will be used to check to build a new subsequence
-        hm = Counter() # a hypothetical map that will be used to check if we
-        # can place a number in an existing sequence
+        # Will use a freq counter to build new subsequences and
+        # subsequence map that records at which a subsequence
+        # ends to see if we can add a new value in
+        freq, ends = Counter(nums), Counter()
 
-        # We want to checked if split the array into consecutive sequences
         for i in nums:
             if freq[i] == 0: # No nums longer available to process
                 continue
 
             freq[i] -= 1
-            # Place number in an existing subsequence if possible
-            if hm[i - 1] > 0:
-                hm[i - 1] -= 1
-                hm[i] += 1
+            # Checks if we can place a number in an existing subsequence
+            if ends[i - 1] > 0:
+                ends[i - 1] -= 1
+                ends[i] += 1
             # Builds a new subsequence of length 3
             elif freq[i + 1] and freq[i + 2]:
                 freq[i + 1] -= 1
                 freq[i + 2] -= 1
-                hm[i + 2] += 1 # Stores number to see if it can be reused
+                ends[i + 2] += 1 # Indicates that we have a subsequence that ends w/ i + 2
             # Otherwise, we can't place our number, so we return false
             else:
                 return False
@@ -30,3 +30,4 @@ class Solution:
 # Solution:
     # 1) https://leetcode.com/problems/split-array-into-consecutive-subsequences/discuss/106514/C++Python-Esay-Understand-Solution/892981
     # 2) https://www.youtube.com/watch?v=uJ8BAQ8lASE
+    # 3) https://leetcode.com/problems/split-array-into-consecutive-subsequences/discuss/1806697/Python-Solution-with-explanation
