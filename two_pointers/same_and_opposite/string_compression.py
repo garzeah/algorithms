8958202f952157a968e6d09c75df736d5434cbe5
Andy Garcia <acgarzeah@gmail.com>
2022-08-19 17:48:07 -0400

    updated probs

---
+++ two_pointers/same_and_opposite/string_compression.py
@@ -0,0 +1,50 @@
+class Solution(object):
+
+    def compress(self, chars):
+
+        length = len(chars)
+
+        # make it a bit faster
+        if length < 2:
+            return length
+
+        # The start position of the contiguous group of characters we are currently reading
+        anchor = 0
+
+        # position to Write Next
+        # we start with 0 then increase it whenever we write to the array
+        write = 0
+
+        # we go through each charcter till we find a pos where the next is not equal to it
+        # then we check if it has appeared more than once using the anchor and write pointers
+        # 1. iterate till we find a different char
+        # 2. record the no. of times the current char was repeated
+        for i, char in enumerate(chars):
+
+            # check if we have reached the end or a different char
+            # check if we are end or the next char != the current
+            if (i + 1) == length or char != chars[i + 1]:
+                # Moves up characters for sequences like aba, if we
+                # didn't do this we would get aab instead of aba
+                chars[write] = char
+                write += 1
+
+                # Check if char has been repeated have been duplicated
+                # if the read pointer is ahead of the anchor pointer
+                if i > anchor:
+                    # Check no. of times char has been repeated
+                    repeated_times = i - anchor + 1
+
+                    # Write the number
+                    for num in str(repeated_times):
+                        chars[write] = num
+                        write += 1
+
+                # Move the anchor to the next char in the iteration
+                anchor = i + 1
+
+        return write
+
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+# Solution: https://leetcode.com/problems/string-compression/discuss/866955/Python-with-comments-(Explained)
