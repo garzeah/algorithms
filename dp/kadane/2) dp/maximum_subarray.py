98e1223aa00e2b26b7ea5b51cc1bba7fb4ff8fbc
Andy Garcia <acgarzeah@gmail.com>
2022-10-18 02:04:22 -0700

    updated probs

--- dp/kadane/2) dp/maximum_subarray.py
+++ dp/kadane/2) dp/maximum_subarray.py
@@ -1,16 +1,20 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        curr_sum = max_sum = nums[0]
+        max_sum = nums[0]
+        curr_sum = 0
 
-        for i in range(1, len(nums)):
-            # We have two choices for each index we need to consider
-            # which is, is it better to start a new subarray or is
-            # it better to keep building the contiguous subarray
-            curr_sum = max(nums[i], nums[i] + curr_sum)
+        for num in nums:
+            # If our curr_sum to the left of our current number is negative,
+            # we want to reset the curr_sum to 0 because we're being greedy
+            # and want to find the maximum subarray
+            if curr_sum < 0:
+                curr_sum = 0
+
+            curr_sum += num
             max_sum = max(max_sum, curr_sum)
 
         return max_sum
 
 # Time Complexity: O(n)
 # Space Complexity: O(1)
-# Solution: https://www.youtube.com/watch?v=2MmGzdiKR9Y
+# Solution: https://www.youtube.com/watch?v=5WZl3MMT0Eg
