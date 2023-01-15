e114c14326cee3762f928bb5a2aa89b60b9a6bbe
Andy Garcia <acgarzeah@gmail.com>
2023-01-15 12:53:22 -0800

    updated prob

--- two_pointers/same_and_opposite/valid_palindrome.py
+++ two_pointers/same_and_opposite/valid_palindrome.py
@@ -5,23 +5,20 @@ class Solution:
 
         while left < right:
             # Sliding left is not alphanumeric
-            if not s[left].isalnum() is False:
+            while left < right and s[left].isalnum() is False:
                 left += 1
-                continue
 
             # Sliding right is not alphanumeric
-            if s[right].isalnum() is False:
+            while left < right and s[right].isalnum() is False:
                 right -= 1
-                continue
 
             # Not a palindrome
             if s[left] != s[right]:
                 return False
 
             # Checking next letter
-            if s[left] == s[right]:
-                left += 1
-                right -= 1
+            left += 1
+            right -= 1
 
         return True
 
