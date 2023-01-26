002cbbd3ea418b548fd72b12109e9dd53adce802
Andy Garcia <acgarzeah@gmail.com>
2023-01-25 22:12:46 -0800

    added new probs

--- dp/unbounded_knapsack/2) memoization/unbounded_knapsack.py
+++ dp/unbounded_knapsack/2) memoization/unbounded_knapsack.py
@@ -1,26 +1,29 @@
 def solve_knapsack(profits, weights, capacity):
-  dp = [[-1 for _ in range(capacity+1)] for _ in range(len(profits))]
-  return solve_knapsack_recursive(dp, profits, weights, capacity, 0)
+  memo = [ [ -1 for x in range(capacity + 1) ] for y in range(len(profits)) ]
+  return helper(profits, weights, capacity, memo, 0)
 
-def solve_knapsack_recursive(dp, profits, weights, capacity, currentIndex):
-  n = len(profits)
-  # base checks
-  if capacity <= 0 or n == 0 or len(weights) != n or currentIndex >= n:
+def helper(profits, weights, capacity, memo, i):
+  if i >= len(profits) or capacity < 0:
     return 0
 
-  # check if we have not already processed a similar sub-problem
-  if dp[currentIndex][capacity] == -1:
-    # recursive call after choosing the items at the currentIndex, note that we
-    # recursive call on all items as we did not increment currentIndex
-    profit1 = 0
-    if weights[currentIndex] <= capacity:
-      profit1 = profits[currentIndex] + solve_knapsack_recursive(
-        dp, profits, weights, capacity - weights[currentIndex], currentIndex)
+  if memo[i][capacity] != -1:
+    return memo[i][capacity]
 
-    # recursive call after excluding the element at the currentIndex
-    profit2 = solve_knapsack_recursive(
-      dp, profits, weights, capacity, currentIndex + 1)
+  # recursive call after choosing the items at the currentIndex,
+  # note that we recursive call on all items as we did not
+  # increment currentIndex
+  profit1 = 0
+  if weights[i] <= capacity:
+    profit1 = profits[i] + helper(profits, weights, capacity - weights[i], memo, i)
 
-    dp[currentIndex][capacity] = max(profit1, profit2)
+  # recursive call after excluding the element at the currentIndex
+  profit2 = helper(profits, weights, capacity, memo, i + 1)
+  memo[i][capacity] = max(profit1, profit2)
+  return memo[i][capacity]
 
-  return dp[currentIndex][capacity]
+def main():
+  print(solve_knapsack([15, 50, 60, 90], [1, 3, 4, 5], 8))
+  print(solve_knapsack([15, 50, 60, 90], [1, 3, 4, 5], 6))
+
+
+main()
