fe011b1f317155d794c357831136730c434265eb
Andy Garcia <acgarzeah@gmail.com>
2023-02-07 00:11:49 -0500

    updated probs

--- dp/knapsack/2) memoization/minimum_subset_sum_difference.py
+++ dp/knapsack/2) memoization/minimum_subset_sum_difference.py
@@ -1,27 +1,19 @@
-def can_partition(num):
-  memo = [ [ -1 for x in range(sum(num) + 1) ] for y in range(len(num)) ]
-  return helper(num, memo, 0, 0, 0)
+def minimum_subarray_sum_difference(nums):
+    memo = [ [ -1 for x in range(sum(nums) + 1) ] for y in range(len(nums)) ]
+    return helper(nums, memo, 0, 0, 0)
 
-def helper(num, memo, i, sum1, sum2):
-  if i >= len(num):
-    return abs(sum1 - sum2)
+def helper(nums, memo, sum1, sum2, i):
+    if i >= len(nums):
+        return abs(sum1 - sum2)
 
-  if memo[i][sum1] != -1:
+    if memo[i][sum1] != -1:
+        return memo[i][sum1]
+
+    # recursive call after including the number at the currentIndex in the first set
+    diff1 = helper(nums, memo, sum1 + nums[i], sum2, i + 1)
+
+    # recursive call after including the number at the currentIndex in the second set
+    diff2 = helper(nums, memo, sum1, sum2 + nums[i], i + 1)
+
+    memo[i][sum1] = min(diff1, diff2)
     return memo[i][sum1]
-
-  # recursive call after including the number at the currentIndex in the first set
-  diff1 = helper(num, memo, i + 1, sum1 + num[i], sum2)
-
-  # recursive call after including the number at the currentIndex in the second set
-  diff2 = helper(num, memo, i + 1, sum1, sum2 + num[i])
-  memo[i][sum1] = min(diff1, diff2)
-
-  return memo[i][sum1]
-
-def main():
-  print("Can partition: " + str(can_partition([1, 2, 3, 9])))
-  print("Can partition: " + str(can_partition([1, 2, 7, 1, 5])))
-  print("Can partition: " + str(can_partition([1, 3, 100, 4])))
-
-
-main()
