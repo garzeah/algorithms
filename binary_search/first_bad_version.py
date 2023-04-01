e4330a05930240357dad280ac8314cd811623da5
Andy Garcia <acgarzeah@gmail.com>
2023-03-31 18:26:43 -0700

    updated prob

--- binary_search/first_bad_version.py
+++ binary_search/first_bad_version.py
@@ -3,17 +3,20 @@
 
 class Solution:
     def firstBadVersion(self, n: int) -> int:
-        start, end = 0, n + 1
+        l, r = 1, n
         res = -1
 
-        while start <= end:
-            mid = (start + end) // 2
+        while l <= r:
+            mid = (l + r) // 2
 
+            # Keep moving down until we find it
             if isBadVersion(mid) == True:
-                res = mid
-                end = mid - 1
+                res = mid # Save previous var
+                r = mid - 1
+            # That means we can trim the left side since
+            # all versions after a bad version are also bad
             else:
-                start = mid + 1
+                l = mid + 1
 
         return res
 
