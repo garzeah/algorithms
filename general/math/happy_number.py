483a53a5a2a2be7b443db3b38c84b7689c76a09c
Andy Garcia <acgarzeah@gmail.com>
2023-04-16 23:29:01 -0700

    updated prob

--- general/math/happy_number.py
+++ general/math/happy_number.py
@@ -2,11 +2,18 @@ class Solution:
     def isHappy(self, num: int) -> bool:
         slow, fast = num, num
 
+        # We are using fast and slow pointers because we want to continuously
+        # do this process until the number equals 1 or loops endlessly in a
+        # cycle that does not include 1. Fast and slow will help us break
+        # this cycle and in the end we can use it to determine whether
+        # we have a happy number or not
         while True:
             slow = self.find_square_sum(slow) # Move one step
             fast = self.find_square_sum(self.find_square_sum(fast)) # Move two steps
 
-            if slow == fast:  # Found the cycle
+            # Have to use fast and slow pointers to break the cycle
+            # other it'll endlessly loop forever
+            if slow == fast:
                 break
 
         return slow == 1  # See if the cycle is stuck on the number '1'
