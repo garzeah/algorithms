1405de980f9e4ad1b3ac95a1a7c98dd0c6092cc2
Andy Garcia <acgarzeah@gmail.com>
2023-04-24 23:22:25 -0700

    updated prob

--- general/cyclic_sort/first_missing_positive.py
+++ general/cyclic_sort/first_missing_positive.py
@@ -4,8 +4,9 @@ class Solution:
       while i < n:
         j = nums[i] - 1
 
-        # Ignore all negative numbers and all numbers
-        # greater than or equal to the length of the array)
+        # Only want to sort if it's within the range of [0, n].
+        # If it isn't, then we can assume the first missing
+        # positive is the first number (1)
         if nums[i] > 0 and nums[i] <= n and nums[i] != nums[j]:
           nums[i], nums[j] = nums[j], nums[i]  # swap
         else:
@@ -15,6 +16,8 @@ class Solution:
         if nums[i] != i + 1:
           return i + 1
 
+        # Edge case for numbers like [1], the first missing
+        # positive would be 2 so we can just return the n + 1
       return n + 1
 
 # Time Complexity: O(n)
